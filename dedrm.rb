7de14a1a33e45293526669a13eaabcd5274861ae
Mikhail Gusarov <dottedmag@dottedmag.net>
2018-05-26 16:49:07 +0200

    Update DeDRM

--- dedrm.rb
+++ dedrm.rb
@@ -1,6 +1,6 @@
 cask 'dedrm' do
-  version '6.5.5'
-  sha256 'c112a3bd1d56cb24cddf0cf280bd3f7d49f8b1a50507c100681aa9c098eac19e'
+  version '6.6.0'
+  sha256 '30bfdf68cc031541ac253c5a01c318c2f357d09ad88d4f5a63f9f42f8563d200'
 
   url "https://github.com/apprenticeharper/DeDRM_tools/releases/download/v#{version}/DeDRM_tools_#{version}.zip"
   appcast 'https://github.com/apprenticeharper/DeDRM_tools/releases.atom'
